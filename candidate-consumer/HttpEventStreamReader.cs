b284abb254dfa793dd52b03939a3ea405e9abf48
davids <davids@pageuppeople.com>
2015-09-22 12:18:55 +1000

    working code both http & tcp, simple commands, 'dnx run http', 'dnx run tcp'

---
+++ candidate-consumer/HttpEventStreamReader.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.ServiceModel.Syndication;
+using System.Threading;
+using System.Xml;
+
+namespace candidate_consumer
+{
+    public class HttpEventStreamReader
+    {
+		private SyndicationLink GetNamedLink(IEnumerable<SyndicationLink> links, string name)
+        {
+            return links.FirstOrDefault(link => link.RelationshipType == name);
+        }
+
+        private Uri GetLast(Uri head)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(head);
+            request.Credentials = new NetworkCredential("admin", "changeit");
+            request.Accept = "application/atom+xml";
+            try
+            {
+                using (var response = (HttpWebResponse) request.GetResponse())
+                {
+                    if (response.StatusCode == HttpStatusCode.NotFound)
+                        return null;
+                    using (var xmlreader = XmlReader.Create(response.GetResponseStream()))
+                    {
+                        var feed = SyndicationFeed.Load(xmlreader);
+                        var last = GetNamedLink(feed.Links, "last");
+                        return (last != null) ? last.Uri : GetNamedLink(feed.Links, "self").Uri;
+                    }
+                }
+            }
+            catch(WebException ex)
+            {
+                if (((HttpWebResponse) ex.Response).StatusCode == HttpStatusCode.NotFound) return null;
+                throw;
+            }
+        }
+
+        private void ProcessItem(SyndicationItem item)
+        {
+            Console.WriteLine(item.Title.Text);
+            //get events
+            var request = (HttpWebRequest)WebRequest.Create(GetNamedLink(item.Links, "alternate").Uri);
+            request.Credentials = new NetworkCredential("admin", "changeit");
+            request.Accept = "application/json";
+            using (var response = request.GetResponse())
+            {
+                var streamReader = new StreamReader(response.GetResponseStream());
+                Console.WriteLine(streamReader.ReadToEnd());
+            }
+        }
+
+        private Uri ReadPrevious(Uri uri)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(uri);
+            request.Credentials = new NetworkCredential("admin", "changeit");
+            request.Accept = "application/atom+xml";
+            using(var response = request.GetResponse())
+            {
+                using(var xmlreader = XmlReader.Create(response.GetResponseStream()))
+                {
+                    var feed = SyndicationFeed.Load(xmlreader);
+                    foreach (var item in feed.Items.Reverse())
+                    {
+                        ProcessItem(item);
+                    }
+                    var prev = GetNamedLink(feed.Links, "previous");
+                    return prev == null ? uri : prev.Uri;
+                }
+            }
+        }
+
+        public void Start()
+        {
+			Console.WriteLine("Press any key to exit.");
+            var stop = false;
+			Uri last = null;
+            while (last == null && !stop)
+            {
+                last = GetLast(new Uri("http://192.168.99.100:2113/streams/candidates"));
+                if(last == null) Thread.Sleep(1000);
+                if (Console.KeyAvailable)
+                {
+                    stop = true;
+                }
+            }
+
+            while(!stop)
+            {
+                var current = ReadPrevious(last);
+                if(last == current)
+                {
+                    Thread.Sleep(1000);
+                }
+                last = current;
+                if(Console.KeyAvailable)
+                {
+                    stop = true;
+                }
+            }
+		}
+    }
+}
--- candidate-consumer/Program.cs
+++ candidate-consumer/Program.cs
@@ -1,10 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using EventStore.ClientAPI;
-using System.Net;
-using System.Text;
 
 namespace candidate_consumer
 {
@@ -12,27 +8,23 @@ namespace candidate_consumer
     {
         public void Main(string[] args)
         {
-			const string STREAM = "candidates";
-            const int DEFAULTPORT = 1113;
-            //uncommet to enable verbose logging in client.
-            var settings = ConnectionSettings.Create();//.EnableVerboseLogging().UseConsoleLogger();
-            using (var conn = EventStoreConnection.Create(settings, new IPEndPoint(IPAddress.Parse("192.168.99.100"), DEFAULTPORT)))
+            if (args[0] == "tcp")
             {
-                conn.ConnectAsync().Wait();
-                //Note the subscription is subscribing from the beginning every time. You could also save
-                //your checkpoint of the last seen event and subscribe to that checkpoint at the beginning.
-                //If stored atomically with the processing of the event this will also provide simulated
-                //transactional messaging.
-                var sub = conn.SubscribeToStreamFrom(STREAM, StreamPosition.Start, true,
-                    (_, x) =>
-                    {
-                        var data = Encoding.ASCII.GetString(x.Event.Data);
-                        Console.WriteLine("Received: " + x.Event.EventStreamId + ":" + x.Event.EventNumber);
-                        Console.WriteLine(data);
-                    });
-                Console.WriteLine("waiting for events. press enter to exit");
-                Console.ReadLine();
-            }
+				// Read using tcp
+				Console.WriteLine("Reading stream using tcp");
+	            new TcpEventStreamReader().Start();
+			}
+            else if (args[0] == "http")
+            {
+				// Read using http
+				Console.WriteLine("Reading stream using http");
+	            new HttpEventStreamReader().Start();
+			}
+
+            Console.WriteLine("waiting for events. press enter to exit");
+			Console.ReadLine();
         }
     }
+
+
 }
